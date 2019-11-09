7f44f21e292d0bbaa068df5467de958beba5dfdd
Niceguy Klim <klim.schukin@jumprampgames.com>
2019-11-09 14:06:25 +0200

    Networking

---
+++ Assets/Codebase/Networking/GetItemsBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using UnityEngine;
+
+public class GetItemsBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        Networking.GetItems((DatabaseItem[] docs) =>
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var databaseItem in docs)
+            {
+                stringBuilder.Append($"{databaseItem.ToString()} \n");
+            }
+            Debug.Log(stringBuilder);
+        }, s => Debug.LogError("Cannot get items :(") );
+    }
+}
---
+++ Assets/Codebase/Networking/NetworkingTypes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+[Serializable]
+public class BaseRequest
+{
+    public string appKey = NetworkingConstants.APP_KEY;
+}
+
+[Serializable]
+public class ModifyLevelRequest : BaseRequest
+{
+    public int levelID;
+    public int minTurnsCount;
+    
+    public ModifyLevelRequest(int lvlID, int newMinTurns)
+    {
+        this.levelID = lvlID;
+        this.minTurnsCount = newMinTurns;
+    }
+}
+
+[Serializable]
+public class ResetDBRequest : BaseRequest
+{
+    public int lvlCount = 100;
+
+    public ResetDBRequest(int lvlCount)
+    {
+        this.lvlCount = lvlCount;
+    }
+}
+
+[Serializable]
+public class ResponseBaseStructure
+{
+    public string message;
+}
+
+[Serializable]
+public class GetItemsResponseStructure : ResponseBaseStructure
+{
+    public DatabaseItem[] content;
+}
+
+[Serializable]
+public class DatabaseItem
+{
+    public string _id;
+    public int levelID;
+    public int minTurnsCount;
+
+    public override string ToString()
+    {
+        return $"levelID:{levelID}, minTurnsCount:{minTurnsCount}";
+    }
+}
+
+public static class NetworkingConstants
+{
+    public const string APP_KEY = "729yfd8as97sdf9yw7894gf7ygbdfs79e67789wyfgyds";
+    public const string ROOT_URL = "https://gentle-coast-35203.herokuapp.com";
+    public const string RESET_DB_ROUTE = "/api/seeddb/";
+    public const string GET_ITEMS_ROUTE = "/api/getitems/";
+    public const string UPDATE_ITEM_ROUTE = "/api/modifylvl/";
+}
