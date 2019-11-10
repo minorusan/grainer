e44a39f72b84d143f0fb66d1c546ce53b6989ff4
Oleh Kopylov <oleh.kopylov@gmail.com>
2019-11-10 14:49:36 +0200

    test levels

---
+++ Assets/Editor/LevelEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(Level))]
+public class LevelEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        Level level = (Level) target;
+       
+        EditorGUI.BeginDisabledGroup(true);
+        DrawDefaultInspector();
+//        if (level.readyToUse)
+//        {
+//            EditorGUILayout.Toggle("Is used", level.isUsed );
+//        }
+//        else
+//        {
+//            EditorGUILayout.Toggle("Ready to use", level.readyToUse );
+//        }
+     
+        EditorGUILayout.ObjectField("Texture", (Texture2D)AssetDatabase.LoadAssetAtPath(level.levelTexturePath, typeof(Texture2D)), typeof(Texture2D), false);
+
+        EditorGUI.EndDisabledGroup();
+        
+    }
+}
