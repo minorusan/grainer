e6b815ae9dafaf3efbd2d0ab5d6f1d434a70d14a
Oleh Kopylov <oleh.kopylov@gmail.com>
2019-11-16 16:44:41 +0200

    win window

---
+++ Assets/Codebase/UI/GameplayTurnsCounterBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameplayTurnsCounterBehaviour : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMesh;
+    private int currentTurnsCount;
+    private void Awake()
+    {
+        currentTurnsCount = 0;
+        GameplayTurnsCountBehaviour.PlayerTurnsCountChanged += IncreaseTurnCount;
+        textMesh.text = currentTurnsCount.ToString();
+    }
+
+    private void IncreaseTurnCount(int count)
+    {
+        currentTurnsCount = count;
+        textMesh.text = currentTurnsCount.ToString();
+    }
+
+    private void OnDisable()
+    {
+        GameplayTurnsCountBehaviour.PlayerTurnsCountChanged -= IncreaseTurnCount;
+    }
+
+    public int GetCurrentTurnsCount()
+    {
+        return currentTurnsCount;
+    }
+}
--- Assets/Codebase/UI/ObjectiveFillBehaviour.cs
+++ Assets/Codebase/UI/ObjectiveFillBehaviour.cs
@@ -1,18 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ObjectiveFillBehaviour : MonoBehaviour
 {
     public Image TrackingImage;
+
+    private float currentFillAmount;
+
     // Start is called before the first frame update
     void Awake()
     {
+        currentFillAmount = 0;
+        TrackingImage.fillAmount = 0;
+        
         GameplayObjectivesBehaviour.ObjectiveStateChanged += GameplayObjectivesBehaviourOnObjectiveStateChanged;
     }
 
     private void GameplayObjectivesBehaviourOnObjectiveStateChanged(float obj)
     {
-        TrackingImage.fillAmount = obj;
+        currentFillAmount = obj;
+    }
+
+    private void FixedUpdate()
+    {
+        if (TrackingImage.fillAmount < currentFillAmount)
+        {
+            TrackingImage.fillAmount = Mathf.Lerp(TrackingImage.fillAmount, currentFillAmount, Time.deltaTime);
+        }
     }
 
     private void OnDisable()
