e424d5122b6b782a91ad8966d34adf7adf8b9327
Niceguy Klim <klim.schukin@jumprampgames.com>
2019-11-16 17:35:27 +0200

    Start screen animations

---
+++ Assets/Codebase/UI/ButtonShakeBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonShakeBehaviour : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            transform.rotation = Quaternion.identity;
+            transform.DOShakeRotation(0.2f, Vector3.one * 7f);
+        });
+    }
+}
--- Assets/Codebase/UI/GameplayTurnsCounterBehaviour.cs
+++ Assets/Codebase/UI/GameplayTurnsCounterBehaviour.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class GameplayTurnsCounterBehaviour : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMesh;
+    public GameObject Container;
+    public float ShakeTime, ShakeMultiplier;
     private int currentTurnsCount;
     private void Awake()
     {
@@ -17,6 +20,9 @@ public class GameplayTurnsCounterBehaviour : MonoBehaviour
 
     private void IncreaseTurnCount(int count)
     {
+        Container.transform.DOKill(true);
+        Container.transform.rotation = Quaternion.identity;
+        Container.transform.DOShakeRotation(ShakeTime, Vector3.one * ShakeMultiplier);
         currentTurnsCount = count;
         textMesh.text = currentTurnsCount.ToString();
     }
