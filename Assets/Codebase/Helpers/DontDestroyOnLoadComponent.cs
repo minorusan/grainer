4fb40ac4244606f80f517e723c8502c8197e23fd
Kliment Schukin <k.schukin@crysberry.com>
2019-12-09 11:00:29 +0200

    sounds

---
+++ Assets/Codebase/Helpers/DontDestroyOnLoadComponent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoadComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
