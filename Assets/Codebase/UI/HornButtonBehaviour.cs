79e9ef0bec277371c0b3610dbd30549bc0849f5c
Kliment Schukin <k.schukin@crysberry.com>
2019-12-10 20:43:18 +0200

    Horn

---
+++ Assets/Codebase/UI/HornButtonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Crysberry.Audio;
+using UnityEngine;
+using UnityEngine.UI;
+using WaterRippleForScreens;
+
+public class HornButtonBehaviour : MonoBehaviour
+{
+    public static event Action<Vector3> HornPlayed = delegate(Vector3 vector3) {  };
+    private Button Button;
+    private RippleEffect RippleEffect;
+    public AudioEffectDefinition HornSound;
+    
+    private void Start()
+    {
+        Button = GetComponent<Button>();
+        RippleEffect = FindObjectOfType<RippleEffect>();
+        Button.onClick.AddListener(PlayHorn);
+    }
+
+    public void PlayHorn()
+    {
+        AudioController.PlayAudio(HornSound);
+        var player = GameObject.FindWithTag("Player");
+        HornPlayed(player.transform.position);
+        var playerScreenPosition = Camera.main.WorldToScreenPoint(player.transform.position);
+        RippleEffect.SetNewRipplePosition(playerScreenPosition);
+        //Button.interactable = false;
+    }
+}
