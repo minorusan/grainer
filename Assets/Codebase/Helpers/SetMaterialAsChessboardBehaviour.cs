7bcb14d18c9ad620f3e47e2f3f9424d0b72040e7
Oleh Kopylov <oleh.kopylov@gmail.com>
2020-01-08 21:01:50 +0200

    Different crop color like on a chessboard

---
+++ Assets/Codebase/Helpers/SetMaterialAsChessboardBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SetMaterialAsChessboardBehaviour : MonoBehaviour
+{
+    [SerializeField] private Material[] materials;
+    [SerializeField] private MeshRenderer[] meshes;
+
+    public void SetMaterial(int i, int j)
+    {
+        var materialNumber = GetMaterialNumber(i, j);
+
+        foreach (var meshRenderer in meshes)
+        {
+            meshRenderer.material = materials[materialNumber];
+        }
+    }
+
+    private int GetMaterialNumber(int i, int j)
+    {
+        return i % 2 == 0 ? j % 2 == 0 ? 0 : 1 : j % 2 == 0 ? 1 : 0;
+    }
+}
