69f01b883154efdea7dbac75312e869127c25401
Oleh Kopylov <oleh.kopylov@gmail.com>
2020-07-19 13:36:28 +0300

    buttons & timescale

---
+++ Assets/UI/EnableButtonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableButtonBehaviour : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SturtupTimerBehaviour.TimerDisabled += OnTimerDisabled;
+    }  
+    private void OnDisable()
+    {
+        SturtupTimerBehaviour.TimerDisabled -= OnTimerDisabled;
+    }
+
+    private void OnTimerDisabled()
+    {
+        GetComponent<Button>().interactable = true;
+    }
+}
