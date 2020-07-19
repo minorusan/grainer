84c65c97505458f739de81fff91027bf2a265c73
Oleh Kopylov <oleh.kopylov@gmail.com>
2020-07-19 19:25:16 +0300

    51-55 lvls

--- Assets/Codebase/Editor/LevelEditor.cs
+++ Assets/Codebase/Editor/LevelEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +7,10 @@ using UnityEngine.SceneManagement;
 [CustomEditor(typeof(Level))]
 public class LevelEditor : Editor
 {
+    int height = 13;
+    int width = 18;
+    static int staticHeight;
+    static int staticwidth;
     public override void OnInspectorGUI()
     {
         var level = (Level) target;
@@ -30,6 +35,32 @@ public class LevelEditor : Editor
             PlayerPrefs.SetInt("EditLevel", level.Number);
             UnityEditor.EditorApplication.isPlaying = true    ;
             EditorSceneManager.OpenScene("Assets/Content/Scenes/level_editor.unity");
+        } 
+        staticHeight = EditorGUILayout.IntField("Weight:", height);
+        staticwidth = EditorGUILayout.IntField("Weight:", width);
+        if (GUILayout.Button("Create levelTexture"))
+        {
+              var texture2 = new Texture2D(staticwidth, staticHeight, TextureFormat.ARGB32, false);
+             texture2.Apply();
+            byte[] bytes = texture.EncodeToPNG();
+            // Object.Destroy(texture);
+            var sceneName = SceneManager.GetActiveScene().name;
+            File.WriteAllBytes(
+                Application.dataPath + "/_Game/Resources/MiniMap/LocationsGroundsMarkers/" + sceneName + "_Marker.png",
+                bytes);
+            string path = "Assets/_Game/Resources/MiniMap/LocationsGroundsMarkers/" + sceneName + "_Marker.png";
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            TextureImporter importer =
+                AssetImporter.GetAtPath("Assets/_Game/Resources/MiniMap/LocationsGroundsMarkers/" + sceneName +
+                                        "_Marker.png") as TextureImporter;
+            importer.wrapMode = TextureWrapMode.Clamp;
+            importer.textureType = TextureImporterType.Sprite;
+            importer.mipmapEnabled = false;
+            importer.maxTextureSize = 1024;
+            importer.textureFormat = TextureImporterFormat.ETC_RGB4Crunched;
+            importer.SaveAndReimport();
+            AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
         }
     }
 }
