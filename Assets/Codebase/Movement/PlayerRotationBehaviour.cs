aa361f47fae4ec7554b3b3255c123b0df6d62dbe
Oleh Kopylov <oleh.kopylov@gmail.com>
2020-07-23 11:04:41 +0300

    Player turns near animals

---
+++ Assets/Codebase/Movement/PlayerRotationBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class PlayerRotationBehaviour : MonoBehaviour
+{
+    public MovementBehaviour Movement;
+    [SerializeField] private Collider rotorCollider;
+    private void Awake()
+    {
+        Movement.OwnerDirectionChanged += OnDirectionChanged;
+        Movement.OwnerWillChangeDirection += OnDirectionWillChange;
+    }
+
+    private void OnDirectionWillChange(GameObject sender, DirectionChangedEventArgs changedeventargs)
+    {
+        rotorCollider.enabled = false;
+    }
+
+    private void OnDirectionChanged(GameObject sender, DirectionChangedEventArgs changedEventArgs)
+    {
+        if (changedEventArgs.Current != MovementDirection.None)
+        {
+            var tweener = transform.DOLookAt(transform.position + changedEventArgs.Current.ToVector3(), 0.3f);
+            tweener.onComplete += () => { rotorCollider.enabled = true; };
+        }
+    }
+}
