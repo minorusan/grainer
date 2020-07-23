9d0a7ddd8bad061f793e3480fe7deb100186755d
Klim Shchukin <k.schukin@jumprampgames.com>
2020-07-23 10:04:41 +0300

    Horizontal animals

---
+++ Assets/Codebase/Entities/AnimalBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimalBehaviour : MonoBehaviour
+{
+    public enum AnimalMoveDirection
+    {
+        Horizontal, Vertical
+    }
+
+    public AnimalMoveDirection Direction;
+    public MovementBehaviour Movement;
+
+
+    private void Update()
+    {
+        if (Direction == AnimalMoveDirection.Horizontal)
+        {
+            if (Movement.IsAbleToMoveInDirection(MovementDirection.Left))
+            {
+                Movement.SetDirection(MovementDirection.Left);
+            }
+            else
+            {
+                Movement.SetDirection(MovementDirection.Right);
+            }
+        }
+    }
+}
